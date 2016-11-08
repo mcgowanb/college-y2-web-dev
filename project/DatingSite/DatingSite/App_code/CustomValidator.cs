e36248c9d1987aacc04afb45d53de6a0ea99ac4d
Brian McGowan <mcgowan.b@gmail.com>
2016-11-08 14:58:54 +0000

    marking state on validators

---
+++ project/DatingSite/DatingSite/App_code/CustomValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace DatingSite.app_code
+{
+    public class CustomValidator : RequestFieldValidatorBase
+    {
+
+        public CustomValidator(String error) : base(error)
+        {
+
+        }
+        protected override bool IsValid(HttpContextBase httpContext, string value)
+        {
+            return true; //some sort of validator
+        }
+    }
+
+    public class MyValidator
+    {
+        public static IValidator Checksum(String errorMessage = null)
+        {
+            if (String.IsNullOrEmpty(errorMessage))
+            {
+                errorMessage = "No Value entered, please try again";
+            }
+            return new CustomValidator(errorMessage);
+        }
+    }
+}
