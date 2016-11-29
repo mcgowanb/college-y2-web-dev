0b655bd6f13c391b0d7bb2c156de2c3342244774
Brian McGowan <mcgowan.b@gmail.com>
2016-11-29 14:55:02 +0000

    Classwork & Project updates

---
+++ wk-9/SessionMGT/SessionMGT/App_code/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SessionMGT.App_code
+{
+    public class Person
+    {
+        public String Name { get; set; }
+    }
+}
